044ebb7764e96bd3dae70351006f32cdb96fe854
Ankit Verma <ankitvermamat@gmail.com>
2019-01-02 06:10:18 +0530

    ss not working for bkg images

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function transform() {
     return `scale(${scale}) rotate(${angle}deg)`
 }
 
-var angle=-20, gapEle=60, gapCon=30,scale=1.20;
+var angle = -20, gapEle = 60, gapCon = 30, scale = 1.20;
 
 window.onload = function () {
     var a = document.getElementById("A");
@@ -13,12 +13,27 @@ window.onload = function () {
     var f = document.getElementById("F");
     var top = document.getElementById("TOP");
     var bottom = document.getElementById("BOTTOM");
+
+    var download = document.getElementById("download");
+
     // top.style.transform = transform()
     // bottom.style.transform = transform()
 
-    b.style.marginLeft=`${gapEle}px`;
-    c.style.marginLeft=`${gapEle}px`;
-    
-    e.style.marginRight=`${gapEle}px`;
-    f.style.marginRight=`${gapEle}px`;
+    b.style.marginLeft = `${gapEle}px`;
+    c.style.marginLeft = `${gapEle}px`;
+
+    e.style.marginRight = `${gapEle}px`;
+    f.style.marginRight = `${gapEle}px`;
+
+    document.getElementById('capture')
+        .addEventListener('click', function (event) {
+            console.log('hello')
+            html2canvas(document.body, {
+                onrendered: function (canvas) {
+                    download.setAttribute('href', canvas.toDataURL("image/png"));
+                    download.setAttribute('download', 'Test file.png');
+                    download.click();
+                }
+            })
+        });
 }
